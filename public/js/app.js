bc1d3748d921d5ff718ef8981b5f069946050970
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-18 23:39:30 -1000

    did some styling, trying to add votes, comments and date section to card

--- public/js/app.js
+++ public/js/app.js
@@ -17,8 +17,8 @@ function createCards(page) {
   cardDiv.className = "card-div";
 
   let cardImg = document.createElement('IMG');
+  cardImg.className = "card-image";
   cardImg.src = page.thumbnail;
-
   let cardImgLink = document.createElement('a');
   cardImgLink.setAttribute("href", page.url);
   cardImgLink.appendChild(cardImg);
@@ -29,7 +29,8 @@ function createCards(page) {
   let titleParagraph = document.createElement('p');
   titleParagraph.innerHTML = page.title;
   titleLink.appendChild(titleParagraph);
-
+  //page.score = number of votes
+  //page.num_comments = number of comments...need to link to comments section
   let authorDiv = document.createElement('div');
   authorDiv.innerHTML = "Submitted By ";
   let authorLink = document.createElement('a');
